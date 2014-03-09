33f4d3e0120400999407bcd0a28deb58c77c8df4
Koo Kyoseung <iam@kyo.seung.info>
2014-03-09 11:08:56 +0900

    include jquery-bpopup.
    include jquery-bpopup.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require pc
+//= require jquery.bpopup.min
--- app/assets/javascripts/pc.js
+++ app/assets/javascripts/pc.js
@@ -1,9 +1,27 @@
 $(document).ready(function(){
     // _gaq.push(['_trackEvent', 'button', 'click', 'img down 1546']);
     // loadJsFile('http://i42.icast-ad.com/track?ccd=1242&mcd=01040601&pcd=1546');
+    /*
   $('#new_user').live('submit', function(e) {
     $.rails.handleRemote( $(this) );
     e.preventDefault();
+  });
+  */
+
+  // by Kyoseung
+  $("#gift_button").click(function(e){
+    e.preventDefault();
+    $("#popup_info").bPopup({
+      modalColor: '#000'
+    });
+  });
+
+$("#popup_info_to_personal").click(function(e){
+    e.preventDefault();
+    $("#popup_personal").bPopup({
+      closeClass: 'b-close-2',
+      modalColor: '#000'
+    });
   });
 });
 
