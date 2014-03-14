8e3c5dd6b643df716bf86d01587da533fbc46a5e
dreamania <dreamania@gmail.com>
2014-03-14 11:41:32 +0900

    모바일 체크박스 iCheck 적용

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require pc
+//= require phone
 //= require jquery.bpopup.min
 //= require jquery.customSelect.min
 //= require icheck.min
---
+++ app/assets/javascripts/phone.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#info_input_radio").iCheck({
+    checkboxClass: 'icheckbox_minimal',
+    radioClass: 'iradio_minimal',
+    increaseArea: '20%' // optional
+  });
+});
