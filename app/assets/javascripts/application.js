c4929ea0c5403d488d6d4c882244bfef83b2534f
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-18 04:11:06 +0900

    제품 자세히 보기

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require pc
+// require turbolinks
 //= require jquery.bpopup.min
 //= require jquery.customSelect.min
 //= require icheck.min
 //= require jquery.selectBox
 //= require jquery.selectric
 //= require jquery.maskedinput.min
+//= require jquery.modal
+//= require pc
