a4cf7087e4106cf80f3ad969e0e6f876155ad8b9
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-18 10:23:43 +0900

    check styling

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -16,11 +16,12 @@
 //= require phone
 //= require jquery.bpopup.min
 //= require jquery.customSelect.min
-// require icheck.min
+//= require jquery.mask
+//= require icheck
+//= require kakao.link.js
+//= require prettyCheckable.min
 // require jquery.selectBox
 // require jquery.selectric
 // require jquery.maskedinput.min
-//= require jquery.mask
 // jquery.meio.mask.js
 // require jquery.inputmask
-//= require kakao.link.js
