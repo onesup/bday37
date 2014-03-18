b2ffa4df019e6d17155860b467f36247035ef820
minivertising양선혜 <sh.yang@minivertising.kr>
2014-03-18 10:07:20 +0900

    mobile phone number masking

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -16,8 +16,11 @@
 //= require phone
 //= require jquery.bpopup.min
 //= require jquery.customSelect.min
-//= require icheck.min
-//= require jquery.selectBox
-//= require jquery.selectric
-//= require jquery.maskedinput.min
+// require icheck.min
+// require jquery.selectBox
+// require jquery.selectric
+// require jquery.maskedinput.min
+//= require jquery.mask
+// jquery.meio.mask.js
+// require jquery.inputmask
 //= require kakao.link.js
