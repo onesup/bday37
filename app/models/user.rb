ab50b916be9f00e1544fd686b0b4d5399cfd8ea7
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-18 15:19:32 +0900

    agree check validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   has_many :messages
   has_one :coupon
+  validates :agree, acceptance: true
   validates :name, presence: true
   validates :phone, presence: true
   validates :phone, uniqueness: true
   validates :birthday, presence: true
-  validates_acceptance_of :agree  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable # :registerable
