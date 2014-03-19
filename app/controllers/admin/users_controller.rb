0165c5ed41b500df6bd0ddfc14f8000d85fa002f
dreamania <dreamania@gmail.com>
2014-03-19 14:21:10 +0900

    Admin:  쿠폰사용자현황 추가

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,4 +4,8 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.order("id desc").page(params[:page]).per(200)
   end
+  
+  def couponused
+    @couponusedusers = User.includes(:coupon).where(:coupons =>{:status => "used"}).order("coupons.updated_at DESC").page(params[:page]).per(200)
+  end
 end
