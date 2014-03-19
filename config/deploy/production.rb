ecc9e52de837b13b21d9f6301f1d0b8820010083
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-19 22:22:24 +0900

    for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'mnv.kr', user: 'deployer', roles: %w{web app db}#, my_property: :my_value
+server 'birthday.mnv.kr', user: 'deployer', roles: %w{web app db}#, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
