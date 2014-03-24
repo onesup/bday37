5887a673e5aa43a3c3bfa93996d43d52d87b40e5
kimpeter <winkim7@gmail.com>
2014-03-24 16:28:04 +0900

    gabba 설

--- app/controllers/web_switch_controller.rb
+++ app/controllers/web_switch_controller.rb
@@ -1,8 +1,8 @@
 class WebSwitchController < ApplicationController
   def index
-    # tracking_id = Rails.application.secrets.ga_tracking_id
-    # url = Rails.application.secrets.url
-    # Gabba::Gabba.new(tracking_id, url).page_view("pc/mobile switch page", "/")
+    tracking_id = Rails.application.secrets.ga_tracking_id
+    url = Rails.application.secrets.url
+    Gabba::Gabba.new(tracking_id, url).page_view("pc/mobile switch page", "/")
     user_agent = UserAgent.parse(request.user_agent)
     if user_agent.mobile?
       device="mobile"
