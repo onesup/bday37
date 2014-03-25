11528ee61f702aa5cc70535f42f87c222758350e
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-25 14:02:04 +0900

    referer 기록 추가

--- app/controllers/fb_switch_controller.rb
+++ app/controllers/fb_switch_controller.rb
@@ -9,13 +9,13 @@ class FbSwitchController < ApplicationController
     user_agent = UserAgent.parse(request.user_agent)
     if user_agent.mobile?
       device="mobile"
-      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device)
+      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: request.referer)
       @traffic_log.save
       redirect_to mobile_index_path({s: params[:s]})
     else
       device="pc"
       s = params[:s] || ""
-      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device)
+      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: request.referer)
       @traffic_log.save
       redirect_to Rails.application.secrets.page_tab_address+"&s="+ s
     end
