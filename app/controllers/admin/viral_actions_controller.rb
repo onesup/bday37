35c643df6df5acb791895f6f2b96d176b17a7a98
kimpeter <winkim7@gmail.com>
2014-03-25 17:56:48 +0900

    어드민 바이럴 등록 합계 테이블 추가

--- app/controllers/admin/viral_actions_controller.rb
+++ app/controllers/admin/viral_actions_controller.rb
@@ -3,5 +3,6 @@ class Admin::ViralActionsController < ApplicationController
   before_action :authenticate_user!
   def index
     @viral_actions = ViralAction.order("id desc").page(params[:page]).per(200)
+    @viral_action_counts = ViralAction.select("sum(case when viral_actions.device = 'pc' then 1 else 0 end) as pc_count, sum(case when viral_actions.device = 'mobile' then 1 else 0 end) as mobile_count, count(*) as total_count")
   end
 end
