0ad82909a6945b12ec69a632df79557528ad69ef
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-25 15:06:48 +0900

    refer 저장 변경

--- app/controllers/fb_switch_controller.rb
+++ app/controllers/fb_switch_controller.rb
@@ -1,20 +1,21 @@
 class FbSwitchController < ApplicationController
   def index
+    referer = URI(request.referer || '').path
     tracking_id = Rails.application.secrets.ga_tracking_id
     url = Rails.application.secrets.url
     g = Gabba::Gabba.new(tracking_id, url)
-    g.referer(request.referer)
+    g.referer(referer)
     g.page_view("fb/mobile switch page", "/")    
     user_agent = UserAgent.parse(request.user_agent)
     if user_agent.mobile?
       device="mobile"
-      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: request.referer)
+      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: referer)
       @traffic_log.save
       redirect_to mobile_index_path({s: params[:s]})
     else
       device="pc"
       s = params[:s] || ""
-      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: request.referer)
+      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: referer)
       @traffic_log.save
       redirect_to Rails.application.secrets.page_tab_address+"?s="+ s
     end
--- app/controllers/web_switch_controller.rb
+++ app/controllers/web_switch_controller.rb
@@ -1,21 +1,21 @@
 class WebSwitchController < ApplicationController
   def index
-    Rails.logger.info "@@@@@referer: " + request.referer.to_s
-    Rails.logger.info "@@@@@referer: " + request.env["HTTP_REFERER"].to_s
+    referer = URI(request.referer || '').path
+    Rails.logger.info "@@@@@referer: "+URI(request.env["HTTP_REFERER"] || '').path
     tracking_id = Rails.application.secrets.ga_tracking_id
     url = Rails.application.secrets.url
     g = Gabba::Gabba.new(tracking_id, url)
-    g.referer(request.referer)
+    g.referer(referer)
     g.page_view("pc/mobile switch page", "/")
     user_agent = UserAgent.parse(request.user_agent)
     if user_agent.mobile?
       device="mobile"
-      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: request.referer)
+      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: referer)
       @traffic_log.save
       redirect_to mobile_index_path({s: params[:s]})
     else
       device="pc"
-      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: request.referer)
+      @traffic_log = TrafficLog.new(ip: request.remote_ip, device: device, referer: referer)
       @traffic_log.save
       redirect_to pc_index_path({s: params[:s]})
     end
