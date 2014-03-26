5e289ac30abfd8897608a1c8ea2e7a494d8e80da
dreamania <dreamania@gmail.com>
2014-03-26 14:47:19 +0900

    Revert "admin: 총합계 추가"
    This reverts commit 186a22d2d51d720375e3d851fae133f72ceab6b3.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,11 +4,8 @@ class Admin::DashboardController < ApplicationController
   
   def index
     @traffic_stats = TrafficLog.select("date(created_at) as created_date,sum(case when device='pc' then 1 else 0 end) as pc_count, sum(case when device='mobile' then 1 else 0 end) as mobile_count").group("date(created_at)").order("date(created_at)")
-    @traffic_stats_sum = TrafficLog.select("sum(case when device='pc' then 1 else 0 end) as pc_count, sum(case when device='mobile' then 1 else 0 end) as mobile_count")
     @viral_stats = ViralAction.select("date(created_at) as created_date,count(*) as viral_count").group("date(created_at)").order("created_at")
-    @viral_stats_sum = ViralAction.select("count(*) as viral_count")
     @user_stats = User.joins(:coupon).select("date(users.created_at) as created_date,sum(case when device='pc' then 1 else 0 end) as pc_count, sum(case when device='mobile' then 1 else 0 end) as mobile_count, sum(case when coupons.status='used' then 1 else 0 end) as coupon_user_count").group("date(created_at)").order("date(created_at)")
-    @user_stats_sum = User.joins(:coupon).select("sum(case when device='pc' then 1 else 0 end) as pc_count, sum(case when device='mobile' then 1 else 0 end) as mobile_count, sum(case when coupons.status='used' then 1 else 0 end) as coupon_user_count")
     
     # @event_stats = @traffic_stats.joins(@viral_stats).where(@traffic_stats.create_date => @viral_stats.created)
   end
--- app/controllers/admin/viral_actions_controller.rb
+++ app/controllers/admin/viral_actions_controller.rb
@@ -3,21 +3,13 @@ class Admin::ViralActionsController < ApplicationController
   before_action :authenticate_user!
   def index
     @viral_actions = ViralAction.order("id desc").page(params[:page]).per(200)
-    @viral_action_counts_daily = ViralAction.select("date(viral_actions.created_at) as created_date,sum(case when viral_actions.device = 'pc' then 1 else 0 end) as pc_count, 
+    @viral_action_counts = ViralAction.select("sum(case when viral_actions.device = 'pc' then 1 else 0 end) as pc_count, 
     	sum(case when viral_actions.device = 'mobile' then 1 else 0 end) as mobile_count, 
-    	count(*) as total_count").group("date(viral_actions.created_at)").order("date(viral_actions.created_at)")
-    @viral_action_counts_sum = ViralAction.select("sum(case when viral_actions.device = 'pc' then 1 else 0 end) as pc_count, 
-      	sum(case when viral_actions.device = 'mobile' then 1 else 0 end) as mobile_count, 
-      	count(*) as total_count")
-    @viral_platform_counts_daily = ViralAction.select("date(viral_actions.created_at) as created_date,sum(case when viral_actions.platform = 'facebook' then 1 else 0 end) as facebook_count, 
+    	count(*) as total_count")
+    @viral_platform_counts = ViralAction.select("sum(case when viral_actions.platform = 'facebook' then 1 else 0 end) as facebook_count, 
     	sum(case when viral_actions.platform = 'twitter' then 1 else 0 end) as twitter_count, 
     	sum(case when viral_actions.platform = 'kakaostory' then 1 else 0 end) as kakaostory_count, 
     	sum(case when viral_actions.platform = 'kakaotalk' then 1 else 0 end) as kakaotalk_count, 
-    	count(*) as total_count").group("date(viral_actions.created_at)").order("date(viral_actions.created_at)")
-      @viral_platform_counts_sum = ViralAction.select("sum(case when viral_actions.platform = 'facebook' then 1 else 0 end) as facebook_count, 
-      	sum(case when viral_actions.platform = 'twitter' then 1 else 0 end) as twitter_count, 
-      	sum(case when viral_actions.platform = 'kakaostory' then 1 else 0 end) as kakaostory_count, 
-      	sum(case when viral_actions.platform = 'kakaotalk' then 1 else 0 end) as kakaotalk_count, 
-      	count(*) as total_count") 
+    	count(*) as total_count") 
   end
 end
