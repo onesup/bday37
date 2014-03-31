244ca9aed54bc5b79127c5f8a404505ff2c65070
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-03-31 15:06:43 +0900

    쿠폰 사용한 유저 날짜별 가져오기 time zone 문제 해결.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,12 +3,25 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   def index
     @users = User.order("id desc").page(params[:page]).per(200)
-    @user_counts = User.select("sum(case when users.device = 'pc' then 1 else 0 end) as pc_count, sum(case when users.device = 'mobile' then 1 else 0 end) as mobile_count, count(*) as total_count")
+    @user_counts = User.select(
+      "sum(case when users.device = 'pc' then 1 else 0 end) as pc_count, 
+      sum(case when users.device = 'mobile' then 1 else 0 end) as mobile_count, 
+      count(*) as total_count")
   end
   
   def couponused
-    @couponusedusers = User.includes(:coupon).where(:coupons =>{:status => "used"}).order("coupons.updated_at DESC").page(params[:page]).per(200)
-    @couponused_counts = User.select("date(coupons.updated_at) used_date,count(*) used_count ").joins(:coupon).where(:coupons =>{:status => "used"}).group("date(coupons.updated_at)").order("date(coupons.updated_at)")
+    @couponusedusers = User.includes(:coupon)
+      .where(:coupons =>{:status => "used"})
+      .order("coupons.updated_at DESC")
+      .page(params[:page]).per(200)
+    @couponused_counts = User.select("
+      date(coupons.updated_at) used_date,
+      count(*) used_count ")
+      .joins(:coupon)
+      .where(:coupons =>{:status => "used"})
+      .group("date(convert_tz(coupons.updated_at,'+00:00','+09:00'))")
+      .order("coupons.updated_at")
+    # @coupon_used_counts = User.all.count_by_date()
   end
   
 end
