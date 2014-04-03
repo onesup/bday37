bfde2e925e5a59519362b163c48b5c13b1d7e9a6
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-04-03 16:37:12 +0900

    admin dashboard viral_stats table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -12,17 +12,12 @@ class Admin::DashboardController < ApplicationController
     @traffic_stats_sum = TrafficLog.select(
       "sum(case when device='pc' then 1 else 0 end) as pc_count
       ,sum(case when device='mobile' then 1 else 0 end) as mobile_count")
-    @viral_stats = ViralAction.select("date(created_at) as created_date, count(*) as viral_count")
-      .group("date(created_at)")
-      .order("created_at")
+    @viral_stats = ViralAction.select(
+      "date(convert_tz(created_at, '+00:00','+09:00')) as created_date, count(*) as viral_count")
+        .group("date(convert_tz(created_at, '+00:00','+09:00'))")
+        .order("created_at")
     @viral_stats_sum = ViralAction.select("count(*) as viral_count")
     @user_stats = User.select(
-      "date(users.created_at) as created_date
-      ,sum(case when device='pc' then 1 else 0 end) as pc_count
-      ,sum(case when device='mobile' then 1 else 0 end) as mobile_count")
-        .group("date(created_at)")
-        .order("date(created_at)")
-    @user_stats1 = User.select(
       "date(convert_tz(users.created_at,'+00:00','+09:00')) as created_date
       ,sum(case when device='pc' then 1 else 0 end) as pc_count
       ,sum(case when device='mobile' then 1 else 0 end) as mobile_count")
