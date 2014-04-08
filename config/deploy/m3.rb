ff0a1df8abafb905c054349df25ac5bf26e99aae
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-04-08 17:12:50 +0900

    deploy

--- config/deploy/m3.rb
+++ config/deploy/m3.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-# role :app, %w{deployer@m3.mnv.kr}
+role :app, %w{deployer@m3.mnv.kr}
 role :web, %w{deployer@m3.mnv.kr}
-# role :db,  %w{deployer@m3.mnv.kr}
+role :db,  %w{deployer@m3.mnv.kr}
 
 # Extended Server Syntax
 # ======================
