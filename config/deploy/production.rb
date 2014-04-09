b760e419fe6a97be19fadbd1573d0262674897fa
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-04-09 13:41:48 +0900

    naver analytics 삭제

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-# role :app, %w{deploy@example.com}
-# role :web, %w{deploy@example.com}
-# role :db,  %w{deploy@example.com}
+role :app, %w{deployer@m4.mnv.kr}
+role :web, %w{deployer@m4.mnv.kr}
+role :db,  %w{deployer@m4.mnv.kr}
 
 # Extended Server Syntax
 # ======================
@@ -14,16 +14,16 @@
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'm4.mnv.kr', user: 'deployer', roles: %w{web app db}, my_property: :my_value
-set :rails_env,   "production"
+# server 'm4.mnv.kr', user: 'deployer', roles: %w{web app db}, my_property: :my_value
+# set :rails_env,   "production"
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
 set :ssh_options, {
-  keys: %w(/Users/daul/.ssh/m4.mnv.kr/id_rsa)
-  # forward_agent: false,
-  # auth_methods: %w(password)
+  keys: %w(/Users/daul/.ssh/m4.mnv.kr/id_rsa),
+  forward_agent: false,
+  auth_methods: %w(password)
 }
 # and/or per server
 # server 'example.com',
