31d99c83695738fac5f41791c9373ded291dd7af
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-04-17 10:53:53 +0900

    fix coupon user counting

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,25 +3,12 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
   def index
     @users = User.order("id desc").page(params[:page]).per(200)
-    @user_counts = User.select(
-      "sum(case when users.device = 'pc' then 1 else 0 end) as pc_count, 
-      sum(case when users.device = 'mobile' then 1 else 0 end) as mobile_count, 
-      count(*) as total_count")
+    @user_counts = User.count_by_device_type
   end
   
   def couponused
-    @couponusedusers = User.includes(:coupon)
-      .where(:coupons =>{:status => "used"})
-      .order("coupons.updated_at DESC")
-      .page(params[:page]).per(200)
-    @couponused_counts = User.select("
-      date(coupons.updated_at) used_date,
-      count(*) used_count ")
-      .joins(:coupon)
-      .where(:coupons =>{:status => "used"})
-      .group("date(convert_tz(coupons.updated_at,'+00:00','+09:00'))")
-      .order("coupons.updated_at")
-    # @coupon_used_counts = User.all.count_by_date()
+    @coupon_used_users = User.coupon_used_users.page(params[:page]).per(200)
+    @coupon_used_counts = User.coupon_used_counts
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,26 @@ class User < ActiveRecord::Base
   # dragonfly_accessor :last_sign_in_ip
   # image_accessor :last_sign_in_ip
   
+  def self.coupon_used_counts
+    result = User.select("
+      date(convert_tz(coupons.updated_at,'+00:00','+09:00')) used_date,
+      count(*) used_count ")
+      .joins(:coupon)
+      .where(coupons:{status: "used"})
+      .group("date(convert_tz(coupons.updated_at,'+00:00','+09:00'))")
+      .order("coupons.updated_at")
+  end
+  
+  def self.coupon_used_users
+    result = User.includes(:coupon)
+      .where(coupons: {status: "used"})
+      .order("coupons.updated_at DESC")
+  end
+  
+  def self.count_by_device_type
+    result = self.select(
+      "sum(case when users.device = 'pc' then 1 else 0 end) as pc_count, 
+      sum(case when users.device = 'mobile' then 1 else 0 end) as mobile_count, 
+      count(*) as total_count")
+  end
 end
